5a39dd458f09cc33e22507e724682cf8a533f0f4
azedede <azedede2028@gmail.com>
2022-10-01 15:24:57 +0000

    backend login register

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')), # index.html in build
     path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
    
 ] 
 
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+# from .views import user_views as views
+from .views import *
 
 
 urlpatterns = [
-  
-   
+    path('login/', MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+
+    path('register/', registerUser, name='register'),
+
+
+    path('<str:pk>/', getUserById, name='user'),
+
+    path('update/<str:pk>/', updateUser, name='user-update'),
+
 ]
