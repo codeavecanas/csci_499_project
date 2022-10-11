898a22a0a2aff7ace6ac702abad7fd7d869e057b
Enrique Ovalles <42857226+EnriqueOv@users.noreply.github.com>
2022-10-11 19:37:19 -0400

    autotrader scraper added

--- cars/scraper.py
+++ cars/scraper.py
@@ -112,7 +112,7 @@ def autotrader_scrape():
     carpic = []
     carengine = []
     
-    pagenum = 3
+    pagenum = 4
     
     headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
