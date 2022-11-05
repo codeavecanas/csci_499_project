c13248bc2127125fb7c8f940fc2eb816d914b072
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-11-05 18:38:54 -0400

    Added car info page and updated styles

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomePageBody from './HomePageBody';
+import NewSearchPage from './NewSearchPage';
 
 const HomePage = () => {
     return (
         <div>
             <HomePageBody />
+            <NewSearchPage />
         </div>
     );
 };
--- frontend/src/components/HowItWorks.js
+++ frontend/src/components/HowItWorks.js
@@ -1,14 +1,14 @@
-import img1 from './HowItWorks/1.png'
-import img2 from './HowItWorks/2.png'
+import img1 from '../images/1.png'
+import img2 from '../images/2.png'
 
-import './HowItWorks.css';
+import '../styles/HowItWorks.css';
 
-function App() {
+function HowItWorks() {
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
         <p>
-        For our Cars 4 U Website, we are using React.js incoroporated with Django and python to read and extract .csv files from car datasets. This allows us to have access to many different caars with different attributes from various datasets. We are also using .json files to fetch the .csv file and convert it so that the uer has all the data and information needed for when they search what kind of car they want. It is filtered by milage, whether the car is combustion engine or electric, make of model, and so on. We actually connect the user to the datbase itself through Django and extrct through React so it's user friendly. The front end part of the website uses React and has a navbar the allows the user to find what they need and filter anthing else out. This makes it easier when getting a car.
+        For our Cars 4 U Website, we are using React.js along with Django and Python to read and extract .json files from car datasets. This allows us to have access to many different cars with different attributes from various datasets. It is filtered by milage, whether the car is combustion engine or electric, make or model, and so on. The front end part of the website uses React and has a navbar that allows the user to find what they need and filter anthing else out. This makes it easier when searching for a car.
         </p>
 
       <img src={img1} width = "300px" height = "200px" />
@@ -20,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default HowItWorks;
